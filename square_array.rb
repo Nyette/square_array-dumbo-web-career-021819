517ba8be57f90730bb72bee6f4d789d464552ca5
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 03:56:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,11 @@ def square_array(numbers)
   # your code here
   new_numbers = []
   numbers.each do |element|
-    new_number = element * element
+    new_number = element**2
     new_numbers << new_number
   end
   return new_numbers
 end
+
 
 
