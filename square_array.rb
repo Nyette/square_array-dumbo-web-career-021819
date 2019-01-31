eb0a38f008a341731596c3a7e5b8e34981154e5f
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-01-31 03:35:49 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  array_of_squares = []
+  array.each do |element|
+    square = element ^ 2
+    array_of_squares.push(square)
+  end
+  return array_of_squares
 end
